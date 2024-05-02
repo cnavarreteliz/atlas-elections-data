af61b99e9883b2166740bc5d964dc7dca8db4625
Carlos Navarrete <cnavarreteliz@gmail.com>
2024-05-02 11:33:39 -0400

    adds metric for polarization

--- run_polarization_chile.sh
+++ run_polarization_chile.sh
@@ -1,12 +1,12 @@
 ## declare an array variable
-years=("2013" "2017" "2021")
+years=("1989" "1993" "1999" "2005" "2009" "2013" "2017" "2021")
 country="Chile"
 level="region_id"
 round="first_round"
+agg="circunscription"
 
 ## now loop through the above array
 for year in "${years[@]}"
 do
-   python pipeline.py -c "$country" -y "$year" -l "$level" -r "$round" -n 999 -f 1
-   python pipeline.py -c "$country" -y "$year" -l "$level" -r "$round" -n 999 -f 0
+   python pipeline.py -c "$country" -y "$year" -l "$level" -r "$round" -n 4 -f 1 -a "$agg"
 done
